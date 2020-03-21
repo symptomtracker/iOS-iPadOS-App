32994140fbb2e3f4d5874d5c7c5abf2a3bc8577b
Wolf Dieter Dallinger <wolf.dieter@dallinger.de>
2020-03-21 17:48:53 +0100

    Initial Commit

---
+++ symptomtracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  symptomtracker
+//
+//  Created by Wolf Dieter Dallinger on 21.03.20.
+//  Copyright © 2020 Wolf Dieter Dallinger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
